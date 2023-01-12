a3bb8ac0ea4854b0a2d051d9480493342b0cf4f7
GitHub Action <action@github.com>
2023-01-12 06:43:23 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "islom-custom-actions"
--- src/main/kotlin/com/github/islombektoshev/islomcustomactions/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/islombektoshev/islomcustomactions/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.islombektoshev.islomcustomactions.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.islombektoshev.islomcustomactions.MyBundle
 
 class MyApplicationService {
 
